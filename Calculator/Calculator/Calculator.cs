1222cff1c60ddeaa1606a2319ccdda2f793cf07c
turki-alotaibi9 <107382355+turki-alotaibi9@users.noreply.github.com>
2023-05-24 16:40:33 +0300

    Add files via upload

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+
+        public void multiply()
+        {
+
+            Console.WriteLine("Welcome To The Multiplication Section");
+
+            Console.WriteLine("Enter first  number ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second  number ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber * secondNumber; 
+            Console.WriteLine("the result is "+result);
+
+        }
+
+        public void divide()
+        {
+            Console.WriteLine("Welcome To the division  Section");
+
+            Console.WriteLine("Enter first  number ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second  number ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber / secondNumber;
+            Console.WriteLine("the result is " + result);
+        }
+        public void subtract() {
+
+            Console.WriteLine("Welcome To The Subtraction Section");
+
+            Console.WriteLine("Enter first  number ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second  number ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber - secondNumber;
+            Console.WriteLine("the result is " + result);
+        }
+
+        public void add() {
+
+            Console.WriteLine("Welcome To The Add Section");
+
+            Console.WriteLine("Enter first  number ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second  number ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber + secondNumber;
+            Console.WriteLine("the result is " + result);
+
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();  
+
+          
+           while (true)
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("Welcome in My App ");
+                Console.WriteLine("Select one ");
+                Console.WriteLine("To multiply Two Numbers press 1");
+                Console.WriteLine("To divide two numbers press 2");
+                Console.WriteLine("To Subtract two numbers press 3");
+                Console.WriteLine("To add two numbers press 4");
+                int selectOption = Convert.ToInt32(Console.ReadLine());
+               
+
+                switch (selectOption)
+                {
+
+                    case 1:
+                        cal.multiply();
+                        break;
+                    case 2:
+                        cal.divide();
+                        break;
+                    case 3:
+                        cal.subtract();
+                        break;
+                    case 4:
+                        cal.add();
+                        break;
+                }
+
+                
+            }
+
+        }
+    }
+
+    
+}
